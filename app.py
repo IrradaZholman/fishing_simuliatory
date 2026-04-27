d87faf6cc90e1d9d38670a0f8184db8506d57d12
IrradaZholman <164073065+IrradaZholman@users.noreply.github.com>
2026-04-27 13:24:40 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify
 from train import generate_message
+import os
 
 app = Flask(__name__)
 
@@ -13,4 +14,4 @@ def get_message():
     return jsonify(message)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
