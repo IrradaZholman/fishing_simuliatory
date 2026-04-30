78632f51a4f05fde9627dc8f1977e595232651f0
IrradaZholman <164073065+IrradaZholman@users.noreply.github.com>
2026-04-30 14:42:25 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import requests
 
 app = Flask(__name__)
 
@@ -9,6 +10,22 @@ def login():
 @app.route("/SIMULIATOR")
 def simulator():
     return render_template("SIMULIATOR.html")
+
+@app.route("/api/login", methods=["POST"])
+def api_login():
+    username = request.form.get("username", "")
+    password = request.form.get("password", "")
+
+    r = requests.post(
+        "https://cybersaqshy.42web.io/simulator_login.php",
+        data={
+            "username": username,
+            "password": password
+        },
+        timeout=15
+    )
+
+    return jsonify(r.json())
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
