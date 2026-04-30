c336d72989a196682f4a7f9885af773ab4bceda9
IrradaZholman <164073065+IrradaZholman@users.noreply.github.com>
2026-04-30 12:34:40 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def login():
-    return render_template("index.html")  # templates/index.html
+    return render_template("index.html")
 
 @app.route("/SIMULIATOR")
 def simulator():
-    return render_template("SIMULIATOR.html")  # templates/SIMULIATOR.html
+    return render_template("SIMULIATOR.html")
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
